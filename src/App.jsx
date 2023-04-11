6b819de8d64beb71867eb9b2e8010cb2f37ac38e
Mohmmad-ow <moemessi00020@gmail.com>
2023-04-11 18:02:27 +0300

    built an faq component and some clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Carousel from "./carousel";
 
 
-function Pics({displayedImage}) {
-  let images = [
-    {src: "https://images.pexels.com/photos/3836292/pexels-photo-3836292.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500", text: "waterfall"},
-    {src: "https://images.pexels.com/photos/2792157/pexels-photo-2792157.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500", text: "night sand"},
-    {src: "https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500", text: "flower field"},
-    {src: "https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500", text: "green bird"},
-    {src: "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500", text: "cute cat"}
-  ]
-  
+function FAQ({question, answer}) {
+  const [isToggled, setIsToggled] = useState(false)
+
+  return (
+      <div className="faq-div">
+      <div className="faq-q-sec">
+        <h2 className="faq-q">{question}</h2>
+        <button
+          onClick={() => {
+            setIsToggled(!isToggled);
+          }}
+        >
+          {isToggled ? "+" : "-"}
+        </button>
+      </div>
+      <div>
+        <h3 className={isToggled ? "faq-a-display" : "faq-a-hide"}>
+          {answer}
+        </h3>
+      </div>
+      </div>
+  )
+
+}
+
+
+function FAQs() {
+  let Faq = [
+    {
+      id: 1,
+      q: "Is this a good product?",
+      a: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo dignissimos, maxime enim pariatur cumque perferendis officiis? Molestias voluptatibus illum perferendis unde necessitatibus ex? Sequi incidunt itaque maiores reiciendis aut obcaecati possimus quas.",
+    },
+    {
+      id: 2,
+      q: "How much does it cost?",
+      a: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos accusantium voluptatum, fugiat corporis facilis odit! Laudantium velit nobis cupiditate.",
+    },
+    {
+      id: 3,
+      q: "When can I get it?",
+      a: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure ad nostrum tempore ducimus doloremque dignissimos totam!",
+    },
+  ];
+
+  let list = Faq.map((ele) => {
+    return (
+      <FAQ question={ele.q} answer={ele.a} key={ele.id}/>
+    )
+  })
  
   return (
-    <div key={displayedImage}>
-      <img className='Carousel-image' src={images[displayedImage].src} alt={images[displayedImage].text} />
+    <div className="faq-table">
+      {list}
     </div>
-  )
+  );
 }
-
-
-function Carousel() {
-  const [displayedImage, setDisplayedImage] = useState(0)
-  
-  function turnLeft() {
-    setDisplayedImage(displayedImage == 0 ? 4 : displayedImage - 1)
-  }
-
-  function turnRight() {
-    setDisplayedImage(displayedImage == 4 ? 0 : displayedImage + 1)
-  }
-  return (
-    <div className='carousel'>
-      
-      <button className='carousel-btn' onClick={turnLeft}>Left</button>
-     
-      <Pics displayedImage={displayedImage} />
-      
-      <button className='carousel-btn' onClick={turnRight}>Right</button>
-      
-      
-    </div>
-  )
-}
-
-
 
 function App() {
-
   return (
     <div className="App">
-       <Carousel />
+      <FAQs />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
